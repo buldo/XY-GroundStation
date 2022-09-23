a7c3bc2c9b2109572984a5e00753c6d985fa0e91
Roman Buldygin <froooks@gmail.com>
2022-09-23 05:14:13 +0300

    pin assigment

--- firmware/libs/board/include/pin_definitions.hpp
+++ firmware/libs/board/include/pin_definitions.hpp
@@ -4,9 +4,32 @@
 
 #define DISPLAY_SPI_PORT spi1
 
-const int EPD_RST_PIN = 12;
-const int EPD_DC_PIN = 8;
-const int EPD_CS_PIN = 9;
-const int EPD_BL_PIN = 13;
-const int EPD_CLK_PIN = 10;
-const int EPD_MOSI_PIN = 11;
+const int UART_TX_PIN      =  0;
+const int UART_RX_PIN      =  1;
+const int KEY_UP_PIN       =  2;
+const int KEY_ENTER_PIN    =  3;
+const int UNASSIGNED_PIN04 =  4;
+const int UNASSIGNED_PIN05 =  5;
+const int UNASSIGNED_PIN06 =  6;
+const int UNASSIGNED_PIN07 =  7;
+const int EPD_DC_PIN       =  8;
+const int EPD_CS_PIN       =  9;
+const int EPD_CLK_PIN      = 10;
+const int EPD_MOSI_PIN     = 11;
+const int EPD_RST_PIN      = 12;
+const int EPD_BL_PIN       = 13;
+const int UNASSIGNED_PIN14 = 14;
+const int KEY_ESC_PIN      = 15;
+const int KEY_LEFT_PIN     = 16;
+const int KEY_END_PIN      = 17;
+const int KEY_DOWN_PIN     = 18;
+const int UNASSIGNED_PIN19 = 19;
+const int KEY_RIGHT_PIN    = 20;
+const int SERVO1_PIN       = 21;
+const int SERVO2_PIN       = 22;
+const int UNASSIGNED_PIN23 = 23;
+const int UNASSIGNED_PIN24 = 24;
+const int UNASSIGNED_PIN25 = 25;
+const int UNASSIGNED_PIN26 = 26;
+const int UNASSIGNED_PIN27 = 27;
+const int UNASSIGNED_PIN28 = 28;
--- firmware/libs/user_interface/include/UserInterface.hpp
+++ firmware/libs/user_interface/include/UserInterface.hpp
@@ -4,6 +4,7 @@
 #include "KeypadKey.hpp"
 #include "MainScreen.hpp"
 
+#include "pin_definitions.hpp"
 #include "pico/stdlib.h"
 #include "lvgl.h"
 #include <array>
@@ -29,13 +30,13 @@ private:
     uint32_t inputDeviceLastKey = 0;
     std::array<KeypadKey, 7> keypadKeys = 
     {
-        KeypadKey{  2, LV_KEY_UP },
-        KeypadKey{ 18, LV_KEY_DOWN },
-        KeypadKey{ 16, LV_KEY_LEFT },
-        KeypadKey{ 20, LV_KEY_RIGHT },
-        KeypadKey{  3, LV_KEY_ENTER },
-        KeypadKey{ 15, LV_KEY_ESC },
-        KeypadKey{ 17, LV_KEY_END },
+        KeypadKey{ KEY_UP_PIN   , LV_KEY_UP },
+        KeypadKey{ KEY_DOWN_PIN , LV_KEY_DOWN },
+        KeypadKey{ KEY_LEFT_PIN , LV_KEY_LEFT },
+        KeypadKey{ KEY_RIGHT_PIN, LV_KEY_RIGHT },
+        KeypadKey{ KEY_ENTER_PIN, LV_KEY_ENTER },
+        KeypadKey{ KEY_ESC_PIN  , LV_KEY_ESC },
+        KeypadKey{ KEY_END_PIN  , LV_KEY_END },
     };
     /* Input device END */
 

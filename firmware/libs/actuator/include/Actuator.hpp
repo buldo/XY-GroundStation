d84a0cf47c6f677088a3538ae681bcdc3102acbe
Roman Buldygin <froooks@gmail.com>
2022-09-26 03:04:05 +0300

    Starting actuator works

--- firmware/libs/actuator/include/Actuator.hpp
+++ firmware/libs/actuator/include/Actuator.hpp
@@ -3,8 +3,17 @@
 class Actuator
 {
 private:
-    /* data */
+    bool isInManualMode;
+    
+    /* Antenna positioning START */
+    int32_t _theta1;
+    int32_t _theta2;
+    uint32_t _az;
+    uint32_t _el;
+    /* Antenna positioning END */
 public:
     Actuator(/* args */);
     ~Actuator();
+    void SetManualMode(bool isManualModeOn);
+    void SetPosition(uint32_t az, uint32_t el);
 };
--- firmware/libs/actuator/src/Actuator.cpp
+++ firmware/libs/actuator/src/Actuator.cpp
@@ -1,9 +1,15 @@
 #include "Actuator.hpp"
 
+#include <cmath>
+
 Actuator::Actuator(/* args */)
 {
 }
 
 Actuator::~Actuator()
 {
+}
+void Actuator::SetManualMode(bool isManualModeOn)
+{
+    isInManualMode = isManualModeOn;
 }
